d615a82fa11df87701822d2e3e333e9ec6c757a8
zeropointnothing <ZeroPointNothing@outlook.com>
2024-11-03 21:28:27 -0500

    base app (init commit)
    whoops forgot to git

---
+++ takali/main.py
@@ -0,0 +1,150 @@
+import argparse
+import json
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" # hide pygame welcome
+from player import Player
+from time import sleep
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--play", help="Play a song, artist, or albumb.", type=int)
+parser.add_argument("-l", "--list", help="List all available songs, albums, or artists.", action="store_true")
+parser.add_argument("-r", "--refresh", help="Refresh Takali's song cache.", action="store_true")
+parser.add_argument("--album", help="Set supported commands to Album Mode.", action="store_true")
+
+def assemble_songs(dir):
+    output = []
+
+    for song in os.listdir(dir):
+        if os.path.isdir(f"{dir}/{song}"):
+            output.extend(assemble_songs(f"{dir}/{song}")) # recursive
+        elif song.split(".")[-1] in ["flac", "mp3", "wav"]:
+            output.append((f"{dir}/{song}", song.split(".")[-1]))
+        # print(song.split("."))
+
+    return output
+
+def generate_cache(dir):
+    songs = assemble_songs(dir)
+    song_meta = []
+
+    plr = Player()
+
+    for song in songs:
+        song_meta.append(plr.get_info(song[0], song[1]))
+    
+    with open("songcache.json", "w") as f:
+        json.dump(song_meta, f)
+
+    plr.exit()
+
+def fetch_cache():
+    if not os.path.exists("songcache.json"):
+        print("fetching metadata...")
+        generate_cache("/home/exii/Music")
+
+    with open("songcache.json", "r") as f:
+        return json.load(f)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    if args.play != None:
+        print("fetching media...")
+        # songs = assemble_songs("/home/exii/Music")
+        # songs = sorted(songs, key=lambda d: d["track"])
+        song_meta = fetch_cache()
+        songs = sorted(song_meta, key=lambda d: d["album"])
+        song = songs[args.play]
+
+        if not args.album:
+            plr = Player()
+            info = plr.play(song["path"], song["type"])
+
+            print(f"Playing {info["title"]} by {info["artist"]}...")
+            print("Press q<ENTER> to quit or h<ENTER> for commands.")
+
+            try:
+                while plr.is_playing():
+                    sleep(1)
+                    user_inp = input("> ")
+
+                    if user_inp.lower() == "q":
+                        raise KeyboardInterrupt("user abort")
+                    elif user_inp.lower() == "p":
+                        plr.pause()
+                        paused = True
+                    elif user_inp.lower() == "u":
+                        plr.resume()
+                        paused = False
+                    elif user_inp.lower() == "+":
+                        plr.change_volume(10)
+                        print(f"Volume now at {round(plr.mixer.get_volume(), 2)}")
+                    elif user_inp.lower() == "-":
+                        plr.change_volume(-10)
+                        print(f"Volume now at {round(plr.mixer.get_volume(), 2)}")
+
+            except KeyboardInterrupt:
+                plr.stop()
+        if args.album:
+            song_meta = fetch_cache()
+            
+            plr = Player()
+            
+            albums = list(dict.fromkeys([_["album"] for _ in song_meta]))
+
+            queue = [_ for _ in song_meta if _["album"] == albums[args.play]]
+            queue = sorted(queue, key=lambda d: d["track"])
+            queue_index = 0
+
+            print(f"Playing album '{albums[args.play]}' by {queue[0]["album_artist"]}...")
+            print("Press q<ENTER> to quit or h<ENTER> for commands.")
+
+            try:
+                while queue_index < len(queue):
+                    paused = False
+                    print(f"Playing {queue[queue_index]["title"]} by {queue[queue_index]["artist"]}...")
+                    plr.play(queue[queue_index]["path"], queue[queue_index]["type"])
+
+                    while plr.is_playing()[1] or paused:
+                        sleep(1)
+                        user_inp = input("> ")
+
+                        if user_inp.lower() == "q":
+                            raise KeyboardInterrupt("user abort")
+                        elif user_inp.lower() == "s":
+                            break
+                        elif user_inp.lower() == "p":
+                            plr.pause()
+                            paused = True
+                        elif user_inp.lower() == "u":
+                            plr.resume()
+                            paused = False
+                        elif user_inp.lower() == "r":
+                            queue_index -= 1
+                            break
+                        elif user_inp.lower() == "+":
+                            plr.change_volume(10)
+                            print(f"Volume now at {round(plr.mixer.get_volume(), 2)}")
+                        elif user_inp.lower() == "-":
+                            plr.change_volume(-10)
+                            print(f"Volume now at {round(plr.mixer.get_volume(), 2)}")
+
+                    plr.stop() # ensure the song is over
+                    queue_index += 1
+            except KeyboardInterrupt:
+                plr.stop()
+
+
+
+    elif args.refresh:
+        print("fetching metadata...")
+        generate_cache("/home/exii/Music")
+    elif args.list:
+        song_meta = fetch_cache()
+        songs = sorted(song_meta, key=lambda d: d["album"])
+
+        print("Your Library:")
+        for i, song in enumerate(songs):
+            print(f"{i} : {song["artist"]} - {song["title"]}")
+
---
+++ takali/player.py
@@ -0,0 +1,131 @@
+import pydub
+import pydub.utils
+from pygame import mixer
+from tempfile import NamedTemporaryFile
+from time import sleep
+
+class Player:
+    def __init__(self):
+        mixer.init()
+        
+        self.mixer = mixer.music
+
+        # Settings
+        self.volume = 100
+
+        # Status
+        self.__playing = False # playing audio
+        self.__active = False # loaded audio, ready to play
+        self.__file = None
+
+        pass
+
+    def play(self, path: str, input_format: str):
+        """
+        Load a file into memory and start playback.
+
+        Automatically converts the input file into a wav, storing it temporarily inside of
+        the system's temp folder via `tempfile`.
+
+        Returns song information.
+        """
+        tmp = NamedTemporaryFile() # create a temp file to write the conversion to
+        self.__file = tmp
+        print(tmp.name)
+
+        audio = pydub.AudioSegment.from_file(path, input_format)
+        info = self.get_info(path, input_format)
+
+        audio.export(self.__file.name, "wav")
+        # audio.export(self.__file, "wav")
+
+
+        self.mixer.load(self.__file.name)
+        self.mixer.play()
+
+        self.__playing = True
+        self.__active = True
+
+        return info
+
+    def get_info(self, path: str, type: str):
+        # fetch metadata
+        audio_info = pydub.utils.mediainfo(path)
+        # audio_info = TinyTag.get(path)
+        tags = audio_info.get("TAG", "???")
+
+        if type == "flac":
+            artist = tags.get("ARTIST", None)
+            album_artist = tags.get("album_artist")
+            album = tags.get("ALBUM", None)
+            title = tags.get("TITLE", None)
+            track = int(tags.get("track", 0))
+            genre = tags.get("GENRE", None)
+        elif type == "mp3":
+            artist = tags.get("artist", None)
+            album_artist = tags.get("album_artist")
+            album = tags.get("album", None)
+            title = tags.get("title", None)
+            track = int(tags.get("track", 0))
+            genre = None
+            
+
+        return {"path": path, "type": type, "artist": artist, "album_artist": album_artist, "album": album, "title": title, "genre": genre, "track": track}
+    
+    def stop(self):
+        """
+        Stop playback and close the input file, if necessary.
+        """
+        self.mixer.stop()
+
+        if self.__file and not self.__file.closed: # ensure temp files are closed properly
+            self.__file.close()
+
+        self.__playing = False
+        self.__active = False
+
+    def exit(self):
+        """
+        Exit the mixer.
+
+        Useful if you only need the player for info gathering.
+        """
+        mixer.quit()
+
+    def pause(self):
+        """
+        Pause playback.
+        """
+        self.mixer.pause()
+        self.__playing = False
+
+    def resume(self):
+        """
+        Resume playback.
+        """
+        self.mixer.unpause()
+        self.__playing = True
+
+    def change_volume(self, by: int):
+        new_vol = self.mixer.get_volume()+(by/100)
+
+        if new_vol <= 0:
+            new_vol = 0.0
+        elif new_vol >= 1.0:
+            new_vol = 1.0
+
+        self.mixer.set_volume(new_vol)
+
+    def is_playing(self):
+        """
+        Whether or not the player is currently playing.
+
+        Returns a tuple of the `Player` status and the status reported by PyGame. These should match.
+        """
+        return (self.__playing, self.mixer.get_busy())
+    
+    def is_active(self):
+        """
+        Whether or not the player has a file loaded and ready to play.
+        """
+        return self.__active
