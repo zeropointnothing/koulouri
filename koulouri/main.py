638fc7e3377a47c1b1d7f4ef29d616be9d63eeba
zeropointnothing <ZeroPointNothing@outlook.com>
2025-01-07 09:08:30 -0500

    update version

--- koulouri/main.py
+++ koulouri/main.py
@@ -16,7 +16,7 @@ parser.add_argument("-v", "--version", help="Print Koulouri's version, then exit
 parser.add_argument("--album", help="Set supported commands to Album Mode.", action="store_true")
 parser.add_argument("--add-source", help="Add a folder to your library.", action="append")
 
-VERSION = "2.0.0"
+VERSION = "2.1.0"
 
 def assemble_songs(dir):
     output = []
